63d3f32cce1d3ea7ff91b2bfde8089a59d53b3d2
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-01-24 20:54:39 +0200

    ....

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
+  name:string = "Clean Code"
+  author:string = "Robert C Martin"
+  src:string = "https://m.media-amazon.com/images/I/51uO-K+V5dL._AC_SY1000_.jpg"
 
+  name2 ="Pragmatic Programmer"
+  author2 = "Andrew Hunt"
+  src2 = "https://m.media-amazon.com/images/I/41HXiIojloL._SX396_BO1,204,203,200_.jpg"
 }
