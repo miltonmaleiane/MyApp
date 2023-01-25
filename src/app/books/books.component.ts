94ee2def8e6ed839c25c8eb728dfb94e5b61db98
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-01-25 13:34:02 +0200

    NGFOR && NGIF

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,27 +1,51 @@
 import { Component } from '@angular/core';
-
+interface Book{
+  name : string;
+  author : string;
+  image:string;
+  amount: number;
+}
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent {
-  name:string = "Clean Code"
-  author:string = "Robert C Martin"
-  src:string = "https://m.media-amazon.com/images/I/51uO-K+V5dL._AC_SY1000_.jpg"
 
-  name2 ="Pragmatic Programmer"
-  author2 = "Andrew Hunt"
-  src2 = "https://m.media-amazon.com/images/I/41HXiIojloL._SX396_BO1,204,203,200_.jpg"
+export class BooksComponent {
+
+  books:Book[] = [
+    {
+    name: "clean code",
+    author:"robert c martin",
+    image:"https://m.media-amazon.com/images/I/51uO-K+V5dL._AC_SY1000_.jpg",
+    amount: 350
+    },
+    {
+      name:"pragmatic programmer",
+      author:  "andrew hunt",
+      image:"https://m.media-amazon.com/images/I/41HXiIojloL._SX396_BO1,204,203,200_.jpg",
+      amount: 89.99
+    },
+    
+
+  ]
+  
+
+
 
   isDisabled:boolean = false;
   isDisabled2:boolean = false;
+  showBooks: boolean = true;
+  isShowing: boolean = false;
 
   handleClick(){
     this.isDisabled = true; 
    // alert("Button disabled ")
-
   }
+  toggleBooks(){
+
+   }
+   
   myName: string =""
   
 }
