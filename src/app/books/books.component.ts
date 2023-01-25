ddec367b4c69fc127f1c95ab84f07bb5f64e6192
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-01-25 14:57:17 +0200

    Styling Components

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -26,7 +26,18 @@ export class BooksComponent {
       image:"https://m.media-amazon.com/images/I/41HXiIojloL._SX396_BO1,204,203,200_.jpg",
       amount: 89.99
     },
-    
+    {
+      name: "The Art of Programming",
+      author:"Donald E. KNUTH",
+      image:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/ArtOfComputerProgramming.svg/1200px-ArtOfComputerProgramming.svg.png",
+      amount: 18300
+      },
+      {
+        name:"Introduction To Algorithms",
+        author:  "T Cormen",
+        image:"https://m.media-amazon.com/images/I/41HXiIojloL._SX396_BO1,204,203,200_.jpg",
+        amount: 1500
+      },
 
   ]
   
