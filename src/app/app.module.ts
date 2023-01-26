ed4ca75f9c9bdcb3bf82e959f6ddf69cd5c94ac1
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-01-26 15:44:33 +0200

    Custom Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BooksModule } from './books/books.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { FormsModule } from '@angular/forms';
-import { BookComponent } from './book/book.component';
+import { BookComponent } from './books/book/book.component';
+import { BooksService } from './books/books.service';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent,
-    BookComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
+  imports: [ BrowserModule, FormsModule, BooksModule  ],
   bootstrap: [AppComponent]
-})
+}) 
 export class AppModule { }
