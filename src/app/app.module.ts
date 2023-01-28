46966240493d2552e28b3258c2f8eda551109d38
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-01-28 11:35:48 +0200

    Two way Binding for Login forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BooksModule } from './books/books.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
-import { FormsModule } from '@angular/forms';
+
 import { BookComponent } from './books/book/book.component';
 import { BooksService } from './books/books.service';
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -16,7 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     CartComponent,
   ],
-  imports: [ BrowserModule, FormsModule, BooksModule,AppRoutingModule ],
+  imports: [ BrowserModule, BooksModule,AppRoutingModule,AuthModule,FormsModule, ],
   bootstrap: [AppComponent]
 }) 
 export class AppModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { NgModel } from '@angular/forms';
+import { LoginForm } from 'src/app/types/auth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+form:LoginForm ={
+  email:'',
+  password :''
+}
 }
