e68433a8cd84475c7b882b179e8b7ab68ac40723
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-01-28 13:24:13 +0200

    ADDING SUPPORT TO FIREBASE

---
+++ src/app/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBJQZJl9G_Vyoz_q8ot9SIGGuusujz8Pe4",
+    authDomain: "angular14-course-47b0c.firebaseapp.com",
+    projectId: "angular14-course-47b0c",
+    storageBucket: "angular14-course-47b0c.appspot.com",
+    messagingSenderId: "764016536687",
+    appId: "1:764016536687:web:75e1d6b845f331e03d719a"
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
