cc078e7d97dae020434ee1c591eb216bf17dc43e
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-01-28 14:33:48 +0200

    Adding Auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+isAuthenticated:boolean = false;
+
+
+  constructor() {}
+    login(){ }
+    register(){ }
+   
+}
